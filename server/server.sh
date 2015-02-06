cd6c85468d7eb9186ac08b25f0dd271fd0daa48a
shak-mar <shakmar@openmailbox.org>
2015-02-06 17:37:03 +0100

    Clean up data_dir/ at the beginning

--- server/server.sh
+++ server/server.sh
@@ -4,4 +4,6 @@ scripts_dir=$(cd $(dirname $0); pwd)
 data_dir=$scripts_dir/data_dir/
 port=2000
 
+rm $data_dir -r
+
 ncat --keep-open --listen $port --sh-exec "$scripts_dir/handle_supervisor.sh $data_dir"
--- supervisor/supervisor.sh
+++ supervisor/supervisor.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+scripts_dir=$(cd $(dirname $0); pwd)
+
 host=localhost
 port=2000
 
-scripts_dir=$(cd $(dirname $0); pwd)
+data_dir=$scripts_dir/data_dir/
 
-ncat localhost 2000 --sh-exec "$scripts_dir/handle_server.sh $scripts_dir/data_dir/"
+rm $data_dir -r
+
+ncat localhost 2000 --sh-exec "$scripts_dir/handle_server.sh $data_dir"
