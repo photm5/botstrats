4a09aef568b03270b638a3d4d9d97b1b6d658af6
shak-mar <shakmar@openmailbox.org>
2015-02-07 16:44:58 +0100

    Automatically connect sniffer to visualizer

--- sniffer/sniff.sh
+++ sniffer/sniff.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+visualizer_port=2047
+
 scripts_dir=$(cd $(dirname $0); pwd)
 
 mkfifo log 2>/dev/null
 
 cat to_server >/dev/null &
-cat to_client | ./sniff.lua > log
+cat to_client | ./sniff.lua > log &
+
+cat log | nc localhost $visualizer_port
