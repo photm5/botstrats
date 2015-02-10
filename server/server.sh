b9c87e06b6db0d3762c293d7543d23c2ddb41cfd
shak-mar <shakmar@openmailbox.org>
2015-02-10 22:10:43 +0100

    Save gamestate in /tmp tmpfs

--- server/server.sh
+++ server/server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 scripts_dir=$(cd $(dirname $0); pwd)
-data_dir=$scripts_dir/data_dir/
 port=2000
 
-rm $data_dir -r 2>/dev/null
+data_dir=/tmp/botstrats/server
+
+rm -rf $data_dir
 
 ncat --keep-open --listen $port --sh-exec "$scripts_dir/handle_supervisor.sh $data_dir"
--- supervisor/supervisor.sh
+++ supervisor/supervisor.sh
@@ -5,8 +5,8 @@ scripts_dir=$(cd $(dirname $0); pwd)
 host=localhost
 port=2001
 
-data_dir=$scripts_dir/data_dir/
+data_dir=/tmp/botstrats/supervisor
 
-rm $data_dir -r 2>/dev/null
+rm -rf $data_dir
 
 ncat localhost $port --sh-exec "$scripts_dir/handle_server.sh $data_dir"
