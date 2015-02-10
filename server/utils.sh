4f9e6fda1ab533cd691881dda32c389105442357
shak-mar <shakmar@openmailbox.org>
2015-02-10 18:41:59 +0100

    Add distance util function

--- server/utils.sh
+++ server/utils.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+function distance ()
+{
+    a_x=$1
+    a_y=$2
+    b_x=$3
+    b_y=$4
+
+    d_x=$(abs $(($a_x - $b_x)))
+    d_y=$(abs $(($a_y - $b_y)))
+
+    distance=$(echo "sqrt ( $d_x ^ 2 + $d_y ^ 2 )" | bc)
+    unset {a,b}_{x,y}
+}
+
 function robot_distance ()
 {
     a_x=$(cat $data_dir/robots/$1/position/x)
@@ -8,11 +22,7 @@ function robot_distance ()
     b_x=$(cat $data_dir/robots/$2/position/x)
     b_y=$(cat $data_dir/robots/$2/position/y)
 
-    d_x=$(abs $(($a_x - $b_x)))
-    d_y=$(abs $(($a_y - $b_y)))
-
-    distance=$(echo "sqrt ( $d_x ^ 2 + $d_y ^ 2 )" | bc)
-    unset {a,b}_{x,y}
+    distance $a_x $a_y $b_x $b_y
 }
 
 function source_script ()
