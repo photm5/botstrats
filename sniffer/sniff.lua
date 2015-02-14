ce346dbaa7a042c5242852de6151e8e1b9a80835
shak-mar <shakmar@openmailbox.org>
2015-02-14 19:50:31 +0100

    Remove curry complexity

--- sniffer/sniff.lua
+++ sniffer/sniff.lua
@@ -2,34 +2,6 @@
 
 cjson = require 'cjson'
 
-function append ( table_a, element )
-    new_table = {}
-    new_table = { table.unpack ( table_a ) }
-    table.insert ( new_table, element )
-    return new_table
-end
-
-function curry ( fun, num_args, args_list )
-    print_list = args_list or {}
-    if num_args == 1 then
-        return function ( x )
-            if args_list then
-                return fun ( table.unpack ( append ( args_list, x ) ) )
-            else
-                return fun ( x )
-            end
-        end
-    else
-        return function ( x )
-            if args_list then
-                return curry ( fun, num_args - 1, append ( args_list, x ) )
-            else
-                return curry ( fun, num_args - 1, { x } )
-            end
-        end
-    end
-end
-
 function repeated_find ( str, pattern, num )
     start_index = 0
     for i = 1, num do
@@ -43,11 +15,12 @@ end
 size = 500
 ticks = 100000
 
-in_stream = true
 while true do
     line = io.read ()
     if not line then break end
-    line_find = curry ( repeated_find, 3 ) ( line ) ( ' ' )
+    function line_find ( n )
+        return repeated_find ( line, ' ', n )
+    end
     function line_field ( num )
         return string.sub ( line, line_find ( num - 1 ) + 1, line_find ( num ) - 1 )
     end
