58c205ba5ae2662ead1b698a4f880f35d0573bf6
shak-mar <shakmar@openmailbox.org>
2015-06-02 14:16:24 +0200

    Remove lua server

--- server/commands/spawn.lua
+++
@@ -1,3 +0,0 @@
-return function ( supervisor, message )
-    lib.spawn ( supervisor, { type = message [ 1 ] }, message.id )
-end
--- server/lib/distance.lua
+++
@@ -1,3 +0,0 @@
-return function ( a, b )
-    return math.sqrt ( ( a.x - b.x ) ^ 2 + ( a.y - b.y ) ^ 2 )
-end
