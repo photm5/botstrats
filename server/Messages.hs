d8434607a192f4b35d6bf2e7a0c0461b9a66a31b
shak-mar <shakmar@openmailbox.org>
2015-06-30 17:04:18 +0200

    Do not ensure message IDs are UUIDs

--- server/Messages.hs
+++ server/Messages.hs
@@ -4,10 +4,8 @@ import Data.List (intersperse)
 import Data.UUID (UUID, fromString)
 import Data.UUID.V4 (nextRandom)
 import System.IO (Handle, hPutStrLn, hGetLine)
-import Text.Parsec
-import Text.Parsec.String
 
-data Message = Message { mId :: UUID
+data Message = Message { mId :: String
                , command :: String
                , parts :: [String]
                }
@@ -16,20 +14,12 @@ instance Show Message where
     show message = concat . intersperse " " $ (show . mId $ message) : command message : parts message 
 
 parseMessage :: String -> Maybe Message
-parseMessage s = either (const Nothing) Just $ parse messageParser "" s
+parseMessage = parseMessage' . words
 
-messageParser :: Parser Message
-messageParser = do
-    id <- many (noneOf " ")
-    uuid <- maybe (fail "") return $ fromString id
-    char ' '
-    command <- many (noneOf " ")
-    optional $ char ' '
-    parts <- sepBy (many $ noneOf " ") $ char ' '
-    return $ Message uuid command parts
-
-partsParser :: Parser [String]
-partsParser = return []
+parseMessage' :: [String] -> Maybe Message
+parseMessage' [] = Nothing
+parseMessage' (_:[]) = Nothing
+parseMessage' (x:y:zs) = Just $ Message x y zs
 
 sendInvalid :: Message -> Handle -> IO ()
 sendInvalid message handle = send handle $ Message (mId message) "invalid" []
@@ -41,7 +31,7 @@ send handle message = sequence_ $
 newMessage :: String -> [String] -> IO Message
 newMessage command parts = do
     uuid <- nextRandom
-    return $ Message uuid command parts
+    return $ Message (show uuid) command parts
 
 recv :: Handle -> IO (Maybe Message)
 recv handle = do
