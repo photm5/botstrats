04d8c1ee2c36efd1806d48b0d4b43c8358ee29dd
shak-mar <shakmar@openmailbox.org>
2015-07-02 16:23:29 +0200

    Add supervisor cabal setup

--- supervisor/shell.nix
+++ supervisor/shell.nix
@@ -1,5 +1,29 @@
-{ pkgs ? (import <nixpkgs> {}) }:
-with pkgs; stdenv.mkDerivation {
-  name = "shell";
-  buildInputs = [ ncat ];
-}
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { cabal-install, mkDerivation, base, bytestring, directory, filepath, mtl
+      , network, process, stdenv, transformers, unix, uuid
+      }:
+      mkDerivation {
+        pname = "botstrats-supervisor";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        buildDepends = [ cabal-install
+          base bytestring directory filepath mtl network process transformers
+          unix uuid
+        ];
+        homepage = "https://github.com/shak-mar/botstrats";
+        description = "Supervisor for botstrats";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  drv = pkgs.haskell.packages.${compiler}.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
